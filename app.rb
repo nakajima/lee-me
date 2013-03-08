6c7ca6f4fdf32bf29b5f9cbc15a078cc6391d931
Pat Nakajima <patnakajima@gmail.com>
2013-03-07 21:32:41 -0800

    redirec to bruce lee on error

--- app.rb
+++ app.rb
@@ -9,29 +9,33 @@ class LeeMeApp < Sinatra::Application
   end
 
   get '/' do
-    # Get the unescaped 'url' param
-    match = env['REQUEST_URI'].match(/url=([^&]*)/)
-    url = match && match[1]
-    
-    unless url
-      content_type :text
-      return "You need to pass a URL. http://#{request.host}/?url=YOUR_PHOTO_URL"
+    begin
+      # Get the unescaped 'url' param
+      match = env['REQUEST_URI'].match(/url=([^&]*)/)
+      url = match && match[1]
+
+      unless url
+        content_type :text
+        return "You need to pass a URL. http://#{request.host}/?url=YOUR_PHOTO_URL"
+      end
+
+      # Ok, it's jpg
+      content_type 'image/jpg'
+
+      # Get the filename
+      filename = File.basename(url)
+
+      # Maybe we have it already
+      if result = LeeMe.already_correct(filename)
+        return File.read result
+      end
+
+      # Download the new file
+      puts `curl -v -o "#{Dir.pwd}/tmp/#{File.basename(url)}" "#{url}"`
+
+      File.read LeeMe.lean_into_it File.join(Dir.pwd, 'tmp', filename)
+    rescue
+      redirect 'http://tedgambordella.com/blog/wp-content/uploads/2010/12/bruce-lee-picture-large-180x300.jpg'
     end
-
-    # Ok, it's jpg
-    content_type 'image/jpg'
-
-    # Get the filename
-    filename = File.basename(url)
-
-    # Maybe we have it already
-    if result = LeeMe.already_correct(filename)
-      return File.read result
-    end
-
-    # Download the new file
-    puts `curl -v -o "#{Dir.pwd}/tmp/#{File.basename(url)}" "#{url}"`
-
-    File.read LeeMe.lean_into_it File.join(Dir.pwd, 'tmp', filename)
   end
 end
